0870d133a038a813f57084d1e643ee877f9450ef
VEDANT SHARMA <vedantsharmaxtulip@gmail.com>
2025-12-05 23:35:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const board = document.querySelector(".board");
+const startButton=document.querySelector(".btn-start");
+const modal=document.querySelector(".modal");
+const startGameModal=document.querySelector(".start-game");
+const gameOverModal=document.querySelector(".game-over");
+const restartButton=document.querySelector(".btn-restart");
+
+const highScoreElement=document.querySelector("#high-score");
+const scoreElement=document.querySelector("#score");
+const timeElement=document.querySelector("#time");
+
 const blockHeight=50;
 const blockWidth=50;
+
+let highScore=localStorage.getItem("highScore") || 0;
+let score=0;
+let time=`00-00`;
+
+highScoreElement.innerText=highScore;
 
 const cols=Math.floor(board.clientWidth / blockWidth);
 const rows=Math.floor(board.clientHeight / blockHeight);
 let intervalId=null;
+let timeIntervalId=null;
+
 let food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)};
 
 const blocks={};
-const snake=[{x:1,y:3}];/*we will store snake in array of object format.initially size of snake will be 3(1st object is the head of the snake)*/ 
+let snake=[{x:1,y:3}];/*we will store snake in array of object format.initially size of snake will be 3(1st object is the head of the snake)*/ 
 let direction='down';
-
-//for(let i=0;i<rows*cols;i++){
-//    const block=document.createElement("div");
-//    block.classList.add("block");
-//    board.appendChild(block);
-//}
 
 for(let row=0;row<rows;row++){
     for(let col=0;col<cols;col++){
         const block=document.createElement("div");
         block.classList.add("block");
         board.appendChild(block);
-        block.innerText=`${row}-${col}`;
         blocks[ `${row}-${col}` ]=block;
     }
 }
@@ -42,13 +53,37 @@ function render(){
         head={x:snake[0].x-1,y:snake[0].y};
     }
 
+    //wall collision logic
     if(head.x<0 || head.x>=rows || head.y<0 || head.y>=cols){
-        alert("Game Over");
-        clearInterval(intervalId)
+        clearInterval(intervalId);
+        modal.style.display="flex";
+        startGameModal.style.display="none";
+        gameOverModal.style.display="flex";
+        return;
+    }
+    if (snake.some(segment => segment.x === head.x && segment.y === head.y)) {
+        clearInterval(intervalId);
+        clearInterval(timeIntervalId);
+        modal.style.display = "flex";
+        startGameModal.style.display = "none";
+        gameOverModal.style.display = "flex";
+        return;
     }
 
+    //food consuming logic
     if(head.x==food.x && head.y==food.y){
         blocks[`${food.x}-${food.y}`].classList.remove("food");
+        food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)};
+        blocks[`${food.x}-${food.y}`].classList.add("food");
+
+        snake.unshift(head);
+        score+=10;
+        scoreElement.innerText=score;
+
+        if(score>highScore){
+            highScore=score;
+            localStorage.setItem("highScore",highScore.toString());
+        }
     }
 
     snake.forEach(segment=>{
@@ -63,9 +98,47 @@ function render(){
     })
 }
 
-intervalId=setInterval(()=>{
-    render();
-},400);
+startButton.addEventListener("click",()=>{
+    modal.style.display="none";
+    intervalId=setInterval(()=>{ render() },300);
+    timeIntervalId=setInterval(()=>{
+        let [min,sec]=time.split("-").map(Number);//destructuring-storing the values of an array in the variables(min,sec) (we are dividing 00-00 on the basis of - so it will given an array ["00","00"])
+
+        if(sec==59){
+            min+=1;
+            sec=0;
+        } else{
+            sec+=1;
+        }
+
+        time=`${min}-${sec}`;
+        timeElement.innerText=time;
+    },1000)
+})
+
+restartButton.addEventListener("click",restartGame);
+
+function restartGame(){
+
+    blocks[`${food.x}-${food.y}`].classList.remove("food");
+    snake.forEach(segment=>{
+        blocks[ `${segment.x}-${segment.y}` ].classList.remove("fill")
+    })
+
+    score=0;
+    time=`00-00`;
+    scoreElement.innerText=score;
+    timeElement.innerText=time;
+    highScoreElement.innerText=highScore;
+
+    modal.style.display="none";
+    direction="down";
+    snake=[{x:1,y:3}];
+    food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)};
+    intervalId=setInterval(()=>{ render() },300);
+}
+
+
 
 window.addEventListener("keydown",(event)=>{
     console.log(event.key);
